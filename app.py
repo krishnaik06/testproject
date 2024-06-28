dc0f2cc3d85a458b2fcbce315884cdc4359e0767
Krish C Naik <krishnaik06@gmail.com>
2024-06-28 23:55:16 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ with open('label_encoder_geo.pkl', 'rb') as file:
 with open('label_encoder_gender.pkl', 'rb') as file:
     label_encoder_gender = pickle.load(file)
 
-with open('experiments/scaler.pkl', 'rb') as file:
+with open('scaler.pkl', 'rb') as file:
     scaler = pickle.load(file)
 
 # Streamlit app
